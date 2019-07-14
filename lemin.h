2eedbf0a568b59ba4a18d874760043e64c6e51c5
Grégoire Drion <gdrion@e-r10-p7.s19.be>
2019-07-14 17:18:28 +0200

    Ajout de swap_rooms dans .h

--- lemin.h
+++ lemin.h
@@ -6,7 +6,7 @@
 /*   By: wdeltenr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 17:01:24 by wdeltenr          #+#    #+#             */
-/*   Updated: 2019/07/13 20:05:49 by gdrion           ###   ########.fr       */
+/*   Updated: 2019/07/14 17:17:44 by gdrion           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ int					free_anthill(t_hill *anthill);
 **
 */
 int					parse_links(t_hill *hill, t_rooms **tab, char *line);
+void				swap_rooms(t_hill *hill, t_rooms **tab, int i, int room_ind);
 
 void				suurballe(t_hill *hill, t_rooms **tab);
 void				short_path(t_hill *hill, t_rooms **tab);
