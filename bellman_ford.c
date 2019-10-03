bb78bdd47011d53f059a3556442136784a92c053
William Deltenre <wdeltenr@e-r3-p4.s19.be>
2019-10-03 17:22:08 +0200

    bellman ford: structure code

--- bellman_ford.c
+++ bellman_ford.c
@@ -5,20 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdeltenr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/01 15:37:24 by wdeltenr          #+#    #+#             */
-/*   Updated: 2019/10/01 16:24:34 by wdeltenr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   bellman_ford.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wdeltenr <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 14:38:32 by wdeltenr          #+#    #+#             */
-/*   Updated: 2019/10/01 15:37:21 by wdeltenr         ###   ########.fr       */
+/*   Updated: 2019/10/03 17:05:21 by wdeltenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,48 +65,61 @@ static int	used_room(t_rooms *room, t_rooms *pred, t_links *li)
 		return (one_used(room, pred, li));
 }
 
+static int	update_dist(t_rooms *room, t_rooms *pred, int i)
+{
+	int			updated;
+	int			tmp;
+	t_links		*li;
+
+	updated = 0;
+	li = room->links;
+	while (li)
+	{
+		if ((room->used || li->room->used) && li->room->index > 1)
+		{
+			tmp = li->room->pred;
+			li->room->pred = used_room(room, pred, li);
+			if (tmp != li->room->pred)
+				updated = 1;
+		}
+		else if ((room->d + li->w < li->room->d || li->room->d == -1)
+		&& li->room->index != 0 && !li->used && room->d != -1)
+		{
+			li->room->d = room->d + li->w;
+			li->room->pred = i;
+			updated = 1;
+		}
+		li = li->next;
+	}
+	return (updated);
+}
+
 void		bellman_ford(t_rooms **tab, int size)
 {
-	t_links		*li;
 	int			k;
 	int			i;
-	int			check;
+	int			updated;
 
 	k = 1;
 	init_dists(tab);
 	while (k < size)
 	{
-		check = 0;
 		i = 0;
+		updated = 0;
 		while (tab[i])
 		{
 			if (i == 1)
 				i++;
-			while (tab[i] && tab[i]->d == -1 && tab[i]->in == -1 && tab[i]->out == -1)
+			while (tab[i] && tab[i]->d == -1 && tab[i]->in == -1
+			&& tab[i]->out == -1)
 				i++;
 			if (!tab[i])
 				break ;
-			li = tab[i]->links;
-			while (li)
-			{
-				if ((tab[i]->used || li->room->used) && li->room->index > 1)
-					li->room->pred = used_room(tab[i], tab[tab[i]->pred], li);
-				else if ((tab[i]->d + li->w < li->room->d || li->room->d == -1)
-				&& li->room->index > 0 && !li->used && tab[i]->d != -1)
-				{
-					li->room->d = tab[i]->d + li->w;
-					li->room->pred = i;
-					check = 1;
-				}
-				li = li->next;
-			}
+			updated += update_dist(tab[i], tab[tab[i]->pred], i);
 			i++;
 		}
-	//	display_tab2(tab);
-	//	if (!check)
-	//		break ;
+		if (!updated)
+			break ;
 		k++;
 	}
-//	display_room(tab[463]);
-//	display_room(tab[785]);
 }
